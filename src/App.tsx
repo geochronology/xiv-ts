c637cdbec174e0eaf011fd65bfd47d37cbb81a55
geochronology <54387644+geochronology@users.noreply.github.com>
2020-11-06 01:06:29 -0800

    completed semantic UI theme config setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {
+  Grid, Header,
 
-function App() {
+} from 'semantic-ui-react'
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Grid>
+      <Grid.Column>
+        <Grid.Row>
+          <Header as="h2">Hi</Header>
+        </Grid.Row>
+      </Grid.Column>
+    </Grid>
+  )
 }
 
-export default App;
+export default App
